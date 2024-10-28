e61664e2a0d7625ba552ac46106d07727285167e
ilya yurin <ilya.yurin@gmail.com>
2024-10-28 12:20:33 +0300

    +

--- Factory_russia/russian_factory.py
+++ Factory_russia/russian_factory.py
@@ -1,3 +1,4 @@
+# парсинг со скаченных файлов с сайта "Заводы РФ" основных данных и email
 import pandas as pd
 from bs4 import BeautifulSoup
 import lxml
@@ -5,7 +6,6 @@ import re
 from tqdm import tqdm
 import time
  
-count = 0
 client_email = []
 
 path = "C:/Python/russian_factory/data/" 
@@ -25,7 +25,6 @@ for i in  tqdm(range(1,10786)):  #range(1, 10786): 786
     except:
         page = "Нет данных"
         
-
     client_email.append([client_name, email,page,link])
 # Create a DataFrame from the list
 df_client_email = pd.DataFrame(client_email, columns =['client_name', 'email','page','link'])
