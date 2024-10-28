fc9d4113ac3cde2715d322aaa0b799a18c077b9d
ilya yurin <ilya.yurin@gmail.com>
2024-10-28 11:42:24 +0300

    +

--- Factory_russia/factory_details.py
+++ Factory_russia/factory_details.py
@@ -2,21 +2,28 @@ from bs4 import BeautifulSoup
 import pandas as pd
 import re
 import lxml 
+from tqdm import tqdm
+import time
+
 factory_details=[]
+path = "C:/Python/project-1/Factory_russia/TEST/"
 
-for i in range(3,4):
-    with open(f'F:/Pyton/project/{i}.html',"r",encoding="utf-8") as data:
+for i in  tqdm(range(1,10)):  #range(1, 10786): 786
+     time.sleep(0.01)
+     with open(f'{path}{i}.html',"r",encoding="utf-8") as data:
         data = data.read()  
-  
-    soup = BeautifulSoup(data,'lxml')
+     soup = BeautifulSoup(data,'lxml')
+    # Ищем в файле название клиента (будет ключом), ссылку на страницу , тоже будет ключом и ИНН
+     client_name = soup.find("div", class_="title-h1").text
+     link = soup.find(href = re.compile('http')).get("href").replace('/details','')
+     #Если нет ИНН, то значение - "Нет данных"   
+     try:
+         inn = soup.find(text=re.compile("ИНН")).next_element.next_element.text             
+     except:
+         inn= "Нет данных"             
+     #Добавляем все данные в список
+     factory_details.append([client_name,link,inn])  
 
-    client_name = soup.find("div", class_="title-h1").text
-    link = soup.find(href = re.compile('http')).get("href").replace('/details','')
-    block= soup.find("ul", class_='content-list').findAll('span')
-    for i in range(0,len(block),2):
-         content_title =block[i].text
-         content_descr=block[i+1].text
-         factory_details.append([client_name,link,content_title,content_descr])  
-
-print('Готово', factory_details) 
-лишнее
+# Create a DataFrame from the list
+df_factory_details = pd.DataFrame(factory_details, columns =['client_name','link','inn'])
+df_factory_details.to_excel('db_factory_details.xlsx')
