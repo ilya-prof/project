6e49d4c4e0da47a20e9208b943451f4d0d43bc3d
ilya yurin <ilya.yurin@gmail.com>
2024-10-31 23:18:43 +0300

    +

--- CRM_download/inn_list.py
+++ CRM_download/inn_list.py
@@ -8,28 +8,29 @@ from bs4 import BeautifulSoup
 import test
 import test.test_html
 import csv
+import re
+from tqdm import tqdm
+import time
 
 #открыть все ссылки на CRM и сохранить из них ИНН
 #Тестируем на 1 странице
-inn_list = ["INN"]
+inn_list = []
 
-for i in range(1,2):
-    with open(f'CRM_download/Data/region_pages{i}.html', "r", encoding='utf-8') as f:        
+for i in tqdm(range(1,84)):  
+     time.sleep(0.01)
+     with open(f'CRM_download/Data/region_pages{i}.html', "r", encoding='utf-8') as f:        
         data = f.read()
-    soup = BeautifulSoup(data, "lxml")
-    items = soup.find_all("tr")
-    for item in items:
-        inn = item.find("td")
-        # print(inn)
-
-# for link in link:
-#     all_links.append(link.get("href")) 
-
-# df = pd.DataFrame(all_links)
-# df.to_csv('CRM_download/links.csv', index=False)
-
-# # сохраняем в файл
-# with open('CRM_download/test.html', "w", encoding='utf-8') as f:        
-#     f.write(page.text)
-
-#TODO сохранить в файл 
+     soup = BeautifulSoup(data, "lxml")
+     items = soup.find("div", class_="table-responsive").find_all("tr")
+     for item in items:
+        try:
+            button = item.find("button", class_="btn")
+            inn = button["contractorid"]
+            inn_list.append(inn)
+        except:
+            continue    
+ 
+#  сохраняем в файл CSV
+df = pd.DataFrame(inn_list)
+df.columns = ['INN']
+df.to_csv('CRM_download/inn.csv',index=False)
