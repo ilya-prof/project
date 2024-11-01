82e956daa0cc8b5b16f5e1e135607b242c6969a3
ilya yurin <ilya.yurin@gmail.com>
2024-11-01 13:28:11 +0300

    +

--- RBC/rbc.py
+++ RBC/rbc.py
@@ -4,6 +4,7 @@ from bs4 import BeautifulSoup
 import requests
 import openpyxl
 from tqdm import tqdm
+import time
 
 data_list = []
 
@@ -13,14 +14,15 @@ headers = {
 # Делаем цикл:
 book = openpyxl.open('RBC/book.xlsx')
 sheet = book.active
-for row in range(2,15): #sheet.max_row+1)
+for row in tqdm(range(2,sheet.max_row+1)): #sheet.max_row+1)  
+    time.sleep(0.01) 
     url_start = sheet[row][0].value
     
     # Ищем ссылку на страницу компании
     req = requests.get(url_start, headers=headers)
     soup=BeautifulSoup(req.text,"lxml")
     url = soup.find("a", class_="company-name-highlight").get("href")
-    sleep(1)
+    sleep(2)
 
     # Проходим на страницу компании
     try:
@@ -29,23 +31,24 @@ for row in range(2,15): #sheet.max_row+1)
         soup2 = BeautifulSoup(req2.text,"lxml")
     except:
         break
-    #Находим ИНН
+    #Находим ИНН, ОГРН и название
     pre_inn = soup2.title.text
     inn = pre_inn[pre_inn.find("ИНН ")+4:pre_inn.find(" — адрес")].strip()
+    name = pre_inn[:pre_inn.find(" — ")].strip() 
+    ogrn = pre_inn[pre_inn.find("ОГРН")+5:pre_inn.find(",")].strip()
     # Находим email
     contacts = soup2.find_all('span', class_="copy-text")
     for item in contacts:
         if '@' in item.text: 
-            email = item.text.strip()
+            email = item.text.strip().lower()
             break
         else:
             email = "Нет"
-    sheet[row][3].value = email
-    sheet[row][2].value = inn
-    print(inn,email)
+    
     # Все заводим в список
-    data_list.append([inn,email])
-    book.save('RBC/test.xlsx')
+    data_list.append([name,ogrn,inn,email])
+    df_email = pd.DataFrame(data_list, columns =['name','ogrn','inn','email'])
+    df_email.to_excel('RBC/RBC_email.xlsx', index=True)
 # Create a DataFrame from the list
-df_email = pd.DataFrame(data_list, columns =['inn','email'])
-df_email.to_excel('RBC/RBC_email.xlsx', index=True)
+# df_email = pd.DataFrame(data_list, columns =['name','inn','email'])
+# df_email.to_excel('RBC/RBC_email.xlsx', index=True)
