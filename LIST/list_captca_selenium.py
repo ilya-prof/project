67b9003565092d2a0cd4303d357e7596bded80d5
ilya yurin <ilya.yurin@gmail.com>
2024-11-25 15:09:04 +0300

    Новые методы Selenium

--- LIST/list_captca_selenium.py
+++ LIST/list_captca_selenium.py
@@ -1,56 +1,18 @@
-from openpyxl import Workbook
-
-from selenium import webdriver
-from bs4 import BeautifulSoup
-import requests
 from time import sleep
-from selenium.webdriver.chrome.options import Options
+from selenium import webdriver
+from selenium.webdriver.common.by import By
 
+# Открытие браузера
+driver = webdriver.Chrome()
 
-o = Options()
-o.add_experimental_option("detach", True)
-driver = webdriver.Chrome(options=o)
-driver.get("file:///D:/Download/List-Org.html")
+# Переход на нужную страницу
+driver.get("https://www.list-org.com/company/14020211")
+sleep(2)
+# Нахождение кнопки по CSS-селектору
+box = driver.find_element(By.XPATH, '//*[@id="recaptcha-anchor"]')
+box.click()
+sleep (2)
 
-text_title = driver.find_element_by_xpath("")
-print (text_title)
-
-
-
-# url_start = sheet.cell(row=row, column=3).value  
-# req = requests.get(url_start, headers=headers)
-# sleep(1)
-# soup=BeautifulSoup(req.text,"lxml")
-# try:
-#     url = "https://www.list-org.com/" + soup.find('div', class_="org_list").find("a").get("href")
-# except:
-#     input(r"Нужно пройти Каптчу!!! Продолжить???")
-#     req = requests.get(url_start, headers=headers)
-#     sleep(1)
-#     soup=BeautifulSoup(req.text,"lxml")
-#     url = "https://www.list-org.com/" + soup.find('div', class_="org_list").find("a").get("href")
-# # Проходим на страницу компании
-# req2 = requests.get(url, headers=headers)
-# sleep(1)
-# soup2 = BeautifulSoup(req2.text,"lxml")
-
-# #Находим ИНН, ОГРН и название
-# pre_inn = soup2.title.text
-# inn = pre_inn[pre_inn.find("ИНН ")+4:].strip()
-# client_name = pre_inn[:pre_inn.find(",")].strip() 
-#     # Находим email
-# try:
-#     email = soup2.find('a', class_="wwbw").get("href").replace('mailto:', "").strip()
-# except:
-#     email = "Нет" 
-    
-# data_email.append([client_name,inn,email])
-# df_email = pd.DataFrame(data_email)
-# df_email.to_excel(r'g:\Мой диск\ПР лизинг\База клиентов\Email\List_email.xlsx', index=False)
-
-
-
-
-
-
-
+button = driver.find_element(By.CSS_Selector, "body > div.main > div.content > div > form > input.btn.btn-primary.m-1")
+button.click()
+sleep (2)
